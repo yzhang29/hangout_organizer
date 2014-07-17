c23e830cc4b27b04f1f727b07e8fe7fecec69c72
Cheng <chengma@yahoo-inc.com>
2014-07-17 00:30:54 -0700

    Add create event and view event function

--- src/com/yahoo/apps/hangoutorganizer/LoginActivity.java
+++ src/com/yahoo/apps/hangoutorganizer/LoginActivity.java
@@ -1,40 +1,48 @@
 package com.yahoo.apps.hangoutorganizer;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 import com.codepath.oauth.OAuthLoginActivity;
+import com.parse.FindCallback;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 public class LoginActivity extends OAuthLoginActivity<RestClient> {
-
+	String username = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "com.yahoo.apps.hangoutorganizer", 
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-                }
-        } catch (NameNotFoundException e) {
-
-        } catch (NoSuchAlgorithmException e) {
-
-        }
+		// try {
+		// PackageInfo info = getPackageManager().getPackageInfo(
+		// "com.yahoo.apps.hangoutorganizer",
+		// PackageManager.GET_SIGNATURES);
+		// for (Signature signature : info.signatures) {
+		// MessageDigest md = MessageDigest.getInstance("SHA");
+		// md.update(signature.toByteArray());
+		// Log.d("KeyHash:",
+		// Base64.encodeToString(md.digest(), Base64.DEFAULT));
+		// }
+		// } catch (NameNotFoundException e) {
+		//
+		// } catch (NoSuchAlgorithmException e) {
+		//
+		// }
+		
 		setContentView(R.layout.activity_login);
+		//login();
+
 	}
 
 	// Inflate the menu; this adds items to the action bar if it is present.
@@ -43,27 +51,58 @@ public class LoginActivity extends OAuthLoginActivity<RestClient> {
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
-	
+
 	// OAuth authenticated successfully, launch primary authenticated activity
 	// i.e Display application "homepage"
-    @Override
-    public void onLoginSuccess() {
-    	// Intent i = new Intent(this, PhotosActivity.class);
-    	// startActivity(i);
-    }
-    
-    // OAuth authentication flow failed, handle the error
-    // i.e Display an error dialog or toast
-    @Override
-    public void onLoginFailure(Exception e) {
-        e.printStackTrace();
-    }
-    
-    // Click handler method for the button used to start OAuth flow
-    // Uses the client to initiate OAuth authorization
-    // This should be tied to a button used to login
-    public void loginToRest(View view) {
-        getClient().connect();
-    }
+	@Override
+	public void onLoginSuccess() {
+		// Intent i = new Intent(this, PhotosActivity.class);
+		// startActivity(i);
+	}
 
+	// OAuth authentication flow failed, handle the error
+	// i.e Display an error dialog or toast
+	@Override
+	public void onLoginFailure(Exception e) {
+		e.printStackTrace();
+	}
+
+	// Click handler method for the button used to start OAuth flow
+	// Uses the client to initiate OAuth authorization
+	// This should be tied to a button used to login
+	public void loginToRest(View view) {
+		getClient().connect();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+	}
+
+	public void login(View v) {
+		ParseFacebookUtils.logIn(this, new LogInCallback() {
+			@Override
+			public void done(ParseUser user, ParseException err) {
+				if (user == null) {
+					Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
+				} else if (user.isNew()) {
+					Log.d("MyApp", "User first logged in Facebook!");
+					Toast.makeText(getBaseContext(), "Succfully log in", Toast.LENGTH_SHORT).show();
+					goToMainActivity();
+				} else {
+					Log.d("MyApp", "User logged in through Facebook!");
+					Toast.makeText(getBaseContext(), "Succfully log in", Toast.LENGTH_SHORT).show();
+					goToMainActivity();
+				}
+			}
+		});
+	}
+	
+	private void goToMainActivity(){
+		Intent i = new Intent(this, MainActivity.class);
+		startActivity(i);
+	}
+	
+	
 }
---
+++ src/com/yahoo/apps/hangoutorganizer/MainActivity.java
@@ -0,0 +1,86 @@
+package com.yahoo.apps.hangoutorganizer;
+
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.yahoo.apps.hangoutorganizer.fragments.CreateEventFragment;
+import com.yahoo.apps.hangoutorganizer.fragments.ViewEventFragment;
+import com.yahoo.apps.hangoutorganizer.listener.FragmentTabListener;
+
+public class MainActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		setupTabs();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+
+	private void setupTabs() {
+		ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		actionBar.setDisplayShowTitleEnabled(true);
+		
+		Tab tab1 = actionBar
+				.newTab()
+				.setText("New Event")
+				.setTag("CreateEventFragment")
+				.setTabListener(new FragmentTabListener<CreateEventFragment>(
+								R.id.fl_container, this, "new_event",
+								CreateEventFragment.class));
+		
+
+		actionBar.addTab(tab1);
+		actionBar.selectTab(tab1);
+		
+		Tab tab2 = actionBar
+				.newTab()
+				.setText("View Event")
+				.setTag("ViewEventFragment")
+				.setTabListener(new FragmentTabListener<ViewEventFragment>(
+								R.id.fl_container, this, "view_event",
+								ViewEventFragment.class));
+		
+		actionBar.addTab(tab2);
+
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		CreateEventFragment createEventFragment = (CreateEventFragment) getSupportFragmentManager().findFragmentByTag("new_event");
+		if(requestCode == 1 && resultCode == RESULT_OK){
+			Log.d("ChengMa","location get data: "+data.getStringExtra("location"));
+			createEventFragment.setLocation(data.getStringExtra("location"));
+		}
+	}
+	
+	
+}
